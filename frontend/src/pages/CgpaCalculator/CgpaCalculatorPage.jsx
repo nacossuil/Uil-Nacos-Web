7f9a096b8e7e22ad77d77d172e475d5e62353a87
Nugar <cladeadenugar@gmail.com>
2023-11-13 11:36:15 +0100

    started the  cgpa page, added tailwind css.

--- frontend/src/pages/CgpaCalculator/CgpaCalculatorPage.jsx
+++ frontend/src/pages/CgpaCalculator/CgpaCalculatorPage.jsx
@@ -1,7 +1,22 @@
+import React from "react";
+
 const CgpaCalculatorPage = () => {
   return (
-    <div>cgpaCalculator</div>
-  )
-}
+    <div className="bg-purple-300">
+      {/* CGPA calculator */}
+      <div
+        id="cgpa-container"
+        className="w-[620px] h-[774px] bg-white rounded-[20px] "
+      >
+        {/* CGPA calculator title container */}
+        <div>
+          <h1 className="text-[#212521] leading-[26px] font-bold text-center">
+            CGPA Calculator
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CgpaCalculatorPage;
