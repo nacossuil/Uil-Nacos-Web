7f75d793579a2f2e2f42f2618c6315a8f6421a57
sodiq <sodiqalao38@gmail.com>
2023-11-14 20:50:33 +0100

    shared tasks in home route, testing branch push

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,7 +1,20 @@
+import './HomePage.css'
+
+/** SECTIONS
+ ** hero -- ashraf
+ ** about us
+ * section 1 --ashraf
+ * section 2,3 -- emma
+ * section 4 -- sodiq
+ ** events and bootcamps -- israel
+ ** executives -- israel
+ ** FAQS -- emma
+ */
+
 const HomePage = () => {
   return (
     <div>
-        HomePage
+      HomePage
     </div>
   )
 }
