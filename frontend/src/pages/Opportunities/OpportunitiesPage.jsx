d67dfad530a0dca8acadd0b87aa92a4e3d37b943
Taofeek Abdulazeez <taofeekabdulazeez2020@gmail.com>
2023-11-14 15:53:44 +0100

    Components Splitting
    * Implemented Reusable Card Components and List Components for the Opportunities Section
    * Added some styling based on the figma file (Yet to be completed)

--- frontend/src/pages/Opportunities/OpportunitiesPage.jsx
+++ frontend/src/pages/Opportunities/OpportunitiesPage.jsx
@@ -1,10 +1,35 @@
-import PropTypes from "prop-types";
 import "./Opportunitiespage.css";
+import PropTypes from "prop-types";
+
+const tempCoursesData = [
+  {
+    img: "./../../../src/assets/Opportunities/resources-img1.png",
+    title: "JavaScript best pratices",
+    description:
+      "This course is designed to take your existing JavaScript knowledge and teach you how to leverage patterns and practices to take...",
+    level: "Beginner",
+  },
+  {
+    img: "./../../../src/assets/Opportunities/resources-img2.png",
+    title: "JavaScript best pratices",
+    description:
+      "This course is designed to take your existing JavaScript knowledge and teach you how to leverage patterns and practices to take...",
+    level: "Beginner",
+  },
+  {
+    img: "./../../../src/assets/Opportunities/resources-img3.png",
+    title: "JavaScript best pratices",
+    description:
+      "This course is designed to take your existing JavaScript knowledge and teach you how to leverage patterns and practices to take...",
+    level: "Beginner",
+  },
+];
 
 const OpportunitiesPage = () => {
   return (
     <div className="opportunities">
       <PageIntro />
+      <Courses />
     </div>
   );
 };
@@ -18,14 +43,16 @@ const PageIntro = () => {
       <div className="container">
         <div className="intro__content">
           <div className="intro__content-text">
-            <h1 className="heading">Opportunities for Students</h1>
+            <h1 className="heading-lg">Opportunities for Students</h1>
             <p className="text-md">
               Welcome to our student opportunities hub, where we are dedicated
               to helping you discover exciting opportunities in the world of
               technology and beyond. Explore scholarships, internships,
               hackathons, and more to enhance your academic journey.
             </p>
-            <Button text="Learn more ↓" />
+            <a href="/#" className="btn btn-lg">
+              Learn more ↓
+            </a>
           </div>
           <div className="intro__content-img-box">
             <img
@@ -39,11 +66,56 @@ const PageIntro = () => {
   );
 };
 
-// Re-usable button component (intended to be re-used throughout the entire page)
-const Button = ({ text }) => {
-  return <button className="btn">{text}</button>;
+const Courses = () => {
+  return (
+    <section className="section-courses">
+      <div className="container">
+        <h1 className="heading-md">Shared Career Resources</h1>
+        <CoursesCards />
+      </div>
+    </section>
+  );
 };
 
-Button.propTypes = {
-  text: PropTypes.string,
+const CoursesCards = () => {
+  return (
+    <ul className="courses__cards">
+      {tempCoursesData.map((course, i) => (
+        <CoursesCard
+          key={i}
+          img={course.img}
+          title={course.title}
+          description={course.description}
+          level={course.level}
+        />
+      ))}
+    </ul>
+  );
+};
+
+const CoursesCard = ({ img, title, description, level }) => {
+  return (
+    <li>
+      <a href="\#" className="courses__card">
+        <img src={img} alt="course" />
+        <div className="content">
+          <h3 className="heading-sm">{title}</h3>
+          <p className="text-sm">{description}</p>
+          <div className="justify-flex">
+            <span className="tag">{level}</span>
+            <a href="/#" className="btn btn-md">
+              View course
+            </a>
+          </div>
+        </div>
+      </a>
+    </li>
+  );
+};
+
+CoursesCard.propTypes = {
+  img: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  level: PropTypes.string,
 };
