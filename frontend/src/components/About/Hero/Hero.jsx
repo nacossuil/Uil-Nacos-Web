6d72795dc099c36d01f9ad1960b9d8eaa37a8f32
Jesuferanmi Agboola <117743728+JFstudios03@users.noreply.github.com>
2023-11-26 20:33:04 +0100

    About hero section update

--- frontend/src/components/About/Hero/Hero.jsx
+++ frontend/src/components/About/Hero/Hero.jsx
@@ -6,7 +6,10 @@ import peopleImg from "../../../assets/imgs/people-group.webp"
 const Hero = () => {
   return (
     <div className='hero-about'>
-        
+      <div className="hero-about-cont">
+        <h1 className="hero-about-heading">About NACOS - Unilorin</h1>
+        <img src={peopleImg} alt="bgImg" />
+      </div>
     </div>
   )
 }
