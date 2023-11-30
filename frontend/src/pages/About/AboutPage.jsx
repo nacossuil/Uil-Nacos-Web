045c31b2a76d9c1e6cb66c3ccdc10cb1e8eabed9
Your Name <you@example.com>
2023-11-30 13:12:26 +0100

    Progress...

--- frontend/src/pages/About/AboutPage.jsx
+++ frontend/src/pages/About/AboutPage.jsx
@@ -1,5 +1,6 @@
 import Hero from "../../components/About/Hero/Hero"
 import Main from "../../components/About/Main/Main"
+import Section1 from "../../components/About/Section1/sec1"
 import Values from '../../components/Shared/Values/Values'
 import Executives from '../../components/Home/Executives/Executives'
 import Faqs from "../../components/Shared/FAQs/Faqs"
@@ -9,6 +10,7 @@ const AboutPage = () => {
     <section className=" about-page">
       <Hero />
       <Main />
+      <Section1 />
       <Values />
       <Executives />
       <Faqs />
