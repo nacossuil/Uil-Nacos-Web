3ec29b56e17f4ceec47939c172678b78ae577b41
Banji0x <ibitomihieniola007@gmail.com>
2024-09-05 05:20:26 +0100

    refactor(about):updated information.

--- frontend/src/components/About/Main/Main.jsx
+++ frontend/src/components/About/Main/Main.jsx
@@ -1,28 +1,27 @@
 import "./main.css";
 import Nacos_connect from "../../../assets/imgs/nacos-connect.webp";
-import { BsArrowRight } from "react-icons/bs";
+import {BsArrowRight} from "react-icons/bs";
 
 const Main = () => {
-  return (
-    <div className="about-page-main">
-      <div className="section1">
-        <div className="about">
-          <h4>ABOUT US</h4>
-          <h2>NACOS Unilorin: Your Tech Community</h2>
-          <p>
-            Nigeria Association of Computing Students (NACOS) Unilorin is the
-            umbrella body for Unilorin students in Computer Science,
-            Engineering, IT, and related fields. With a 20+ year legacy and
-            nationwide presence, NACOS is your vibrant community for networking,
-            growth, and innovation..
-          </p>
+    return (
+        <div className="about-page-main">
+            <div className="section1">
+                <div className="about">
+                    <h4>ABOUT US</h4>
+                    <h2>NACOS Unilorin: Your Tech Community</h2>
+                    <p>
+                        Nigeria Association of Computing Students (NACOS) Unilorin is the umbrella body for Unilorin
+                        students in Computer Science, Computer Engineering, Telecommunication Science, Information
+                        Technology and Computer Science Education. With a 20+ year legacy and nationwide presence, NACOS
+                        is your vibrant community for networking, growth, and innovation.
+                    </p>
+                </div>
+                <div className="section1-img">
+                    <img src={Nacos_connect} alt=""/>
+                </div>
+            </div>
         </div>
-        <div className="section1-img">
-          <img src={Nacos_connect} alt="" />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Main;
